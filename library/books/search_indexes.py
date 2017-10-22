5c31bfc3cec44f12c8b301921cdd5b0af2b906d6
Valder Gallo <valdergallo@gmail.com>
2017-10-22 18:59:25 -0200

    setup haystack

---
+++ library/books/search_indexes.py
@@ -0,0 +1,17 @@
+import datetime
+from haystack import indexes
+from books.models import Book
+
+
+class BookIndex(indexes.SearchIndex, indexes.Indexable):
+    name = indexes.CharField(model_attr='name')
+    author = indexes.CharField(model_attr='author')
+    tags = indexes.CharField(model_attr='tags')
+    pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    def get_model(self):
+        return Book
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
--- library/library/settings.py
+++ library/library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3ua#!+$snumqrwigbn=$lzglw!#09f_k4+fb9sz1ou*&_u%xic'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # libs
+    'graphene_django',
+    'haystack',
+    # apps
+    'authors',
+    'books',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+    },
+}
